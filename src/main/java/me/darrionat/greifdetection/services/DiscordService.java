58acd8dcc08bf41c5ad43b1260e1b749755752cb
Darrionat <52856180+Darrionat@users.noreply.github.com>
2022-02-03 16:22:03 -0500

    Update DiscordService

--- src/main/java/me/darrionat/greifdetection/services/DiscordService.java
+++ src/main/java/me/darrionat/greifdetection/services/DiscordService.java
@@ -1,4 +1,15 @@
 package me.darrionat.greifdetection.services;
 
+import me.darrionat.greifdetection.repositories.ConfigRepository;
+
 public class DiscordService {
+    private final ConfigRepository config;
+
+    public DiscordService(ConfigRepository config) {
+        this.config = config;
+    }
+
+    public void sendFlag() {
+
+    }
 }

93385fd01404405a6eaa90b0302ec698f7d17895
Darrionat <52856180+Darrionat@users.noreply.github.com>
2022-02-03 16:22:16 -0500

    Update FlagService

--- src/main/java/me/darrionat/greifdetection/services/FlagService.java
+++ src/main/java/me/darrionat/greifdetection/services/FlagService.java
@@ -1,9 +1,15 @@
 package me.darrionat.greifdetection.services;
 
+import me.darrionat.greifdetection.flags.FlagType;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class FlagService {
+    private final PlaytimeService playtimeService;
+
+    public FlagService(PlaytimeService playtimeService) {
+        this.playtimeService = playtimeService;
+    }
 
     public boolean runChecks(Player p) {
 
@@ -11,7 +17,7 @@ public class FlagService {
     }
 
     // todo more information on flags
-    private void flagPlayer(Player p, Location loc, String reason) {
+    public void flagPlayer(Player p, Location loc, FlagType flagType) {
 
     }
 }
